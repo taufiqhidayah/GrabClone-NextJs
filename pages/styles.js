827926e879f5d92f6ac5a11a0bf9cd6093fc5e5c
taufiqhidayah <hidayaht_taufiq@yahoo.com>
2022-06-11 04:32:59 +0700

    Feature Add : Integrated Component

---
+++ pages/styles.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+export const LabelBottom = styled.p`
+  text-align: center;
+`;
+
+export const TextBold = styled.p`
+  font-weight: 500;
+  line-height: 24px;
+  color: #363a45;
+  margin-top: 32px;
+`;
+export const TextBoldLight = styled.p`
+  font-family: SanomatGrabApp;
+  font-size: 16px;
+  font-weight: 500;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: 1.5;
+  letter-spacing: normal;
+  color: #fff;
+  margin-bottom: 8px;
+`;
+
+export const TextGrey = styled.p`
+  display: -webkit-box;
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  color: #afb0b4;
+  font-size: 16px;
+  margin-bottom: 8px;
+`;
